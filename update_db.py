af65c25e13e1e7441284237bcac97fdb340ffd1e
jackvandeleuv <106289788+jackvandeleuv@users.noreply.github.com>
2022-10-04 14:16:12 -0400

    List of stops/lines now well-formatted

--- update_db.py
+++ update_db.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import bs4
 import sqlite3
+import re
 from typing import List
 
 
@@ -29,9 +30,9 @@ def exploratory_scrape():
     # Specify lxml parser to avoid different defaults on different machines
     soup = bs4.BeautifulSoup(page.text, features='lxml')
     links = soup.find_all('a')  # Gets route and ETA, but not vehicle number
-    for link in links:
-        print(link.string)
-    # print(soup.prettify())
+    # for link in links:
+    #     print(link.string)
+    print(soup.prettify())
 
 
 # This function scrapes the TrueTime Port Authority Arrival Information System page for a list of currently available
@@ -58,7 +59,6 @@ def check_available_directions(route_num: str) -> (bool, bool):
     return outbound, inbound
 
 
-
 # This function takes in a url and tag, and then scrapes the url for the given tag. It returns a list of strings,
 # which contains every string found inside the given tag at the given url.
 def scrape_html_tag(url, tag: str) -> List[str]:
@@ -73,6 +73,32 @@ def scrape_html_tag(url, tag: str) -> List[str]:
     return result
 
 
+# This function takes in a list of stop names and combines each one into a dict entry, with the id as key and the name
+# as the value.
+def zip_stop_id_and_name(url: str, direction: str, r_num: str, r_name: str) -> dict:
+    page = requests.get(url)
+    # Specify lxml parser to avoid different default parsers on different machines
+    soup_object = bs4.BeautifulSoup(page.text, features='lxml')
+    page_links = soup_object.find_all('a', attrs={'href': re.compile(r'^eta.jsp')})
+
+    stop_info = {}
+    for link in page_links:
+        link_address = link.get('href')
+        end_of_link = re.search(r'id=Port\+Authority\+Bus%3A[0-9]+', link_address).group()
+        stop_id = re.search(r'A[0-9]+', end_of_link).group()
+        stop_id = stop_id[1:]
+
+        if not stop_id.isnumeric():
+            raise Exception("Bad Stop Id!")
+
+        stop_id = int(stop_id)
+        stop_name_from_link = link.text
+        stop_name = stop_name_from_link.strip()
+        stop_info[stop_id] = [stop_name, direction, r_num, r_name]
+
+    return stop_info
+
+
 def scrape_dynamic_tag(url, class_name: str) -> List[str]:
     # This is boilerplate code that initializes a Chrome web driver for use by the Selenium library.
     options = Options()
@@ -82,53 +108,48 @@ def scrape_dynamic_tag(url, class_name: str) -> List[str]:
     driver.get(url)
 
     # Scrape the list of available routes from the TrueTime homepage using Selenium.
-    routes = []
-    results = driver.find_elements(By.CLASS_NAME, class_name)
-    for r in results:
-        routes.append(r.text)
+    result = []
+    driver_result = driver.find_elements(By.CLASS_NAME, class_name)
+    for r in driver_result:
+        result.append(r.text)
     driver.quit()
 
-    return routes
+    return result
 
 
-def get_stop_list() -> List[str]:
+def get_stop_list() -> List[dict]:
     url = 'https://truetime.portauthority.org/bustime/wireless/html/home.jsp'
     routes: List[str] = scrape_dynamic_tag(url, 'larger')  # Scrape the route names
-    all_stops = []
+
+    list_of_dicts = []
+
     for route in routes:
-        r_elements = route.split(' ')  # Split the route to get the route num, which is all we need for the next scrape
-        r_num = r_elements[0]  # The 0-index item in the list is the number
+        print('Route:', route)
+        r_elements = route.split('-')  # Split the route to get the route num, and route name
+        r_name = r_elements[1].strip()
+        r_num = r_elements[0].strip()  # The 0-index item in the list is the number
 
         # Check whether inbound and outbound are available for each route
         outbound, inbound = check_available_directions(r_num)
 
+        # INBOUND and OUTBOUND are separate HTML pages that must be scraped separately.
         if outbound:
             url = f'https://truetime.portauthority.org/bustime/wireless/html/selectstop.jsp?route=Port+Authority+Bus%3A{r_num}&direction=Port+Authority+Bus%3AOUTBOUND'
-            out_stops = scrape_html_tag(url, 'a')
-            for string in out_stops:
-                if string is not None:
-                    string = string.strip()
-                    if string.isupper():
-                        all_stops.append(string)
+            out_stop_names_and_ids: dict = zip_stop_id_and_name(url, 'OUTBOUND', r_num, r_name)
+            list_of_dicts.append(out_stop_names_and_ids)
 
         if inbound:
             url = f'https://truetime.portauthority.org/bustime/wireless/html/selectstop.jsp?route=Port+Authority+Bus%3A{r_num}&direction=Port+Authority+Bus%3AINBOUND'
-            in_stops = scrape_html_tag(url, 'a')
-            for string in in_stops:
-                if string is not None:
-                    string = string.strip()
-                    if string.isupper():
-                        all_stops.append(string)
+            in_stop_names_and_ids: dict = zip_stop_id_and_name(url, 'INBOUND', r_num, r_name)
+            list_of_dicts.append(in_stop_names_and_ids)
+
+    return list_of_dicts
 
 
-    print('All stops:')
-    print(len(all_stops))
-    for stop in all_stops:
-        print(stop)
-
-
-
-
-
-get_stop_list()
+cnt = 0
+list_of_dicts = get_stop_list()
+for d in list_of_dicts:
+    for key in d.keys():
+        cnt += 1
+print(cnt)
 # exploratory_scrape()
