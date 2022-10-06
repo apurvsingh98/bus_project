f86bc3d1624b731754d7440c89249ce0746e69f2
jackvandeleuv <106289788+jackvandeleuv@users.noreply.github.com>
2022-10-06 16:23:52 -0400

    Expanded test db

--- QueryDB.py
+++ QueryDB.py
@@ -9,7 +9,7 @@ connection = sqlite3.Connection('transit_data.db')
 cursor = connection.cursor()
 
 # cursor.execute("""SELECT AVG(ETA) FROM ESTIMATES WHERE ETA != 'DUE' AND STOP_ID = 8192 AND ROUTE_ID = '71A'""")
-cursor.execute('SELECT * FROM ESTIMATES')
+cursor.execute('SELECT COUNT(id) FROM ESTIMATES')
 results = cursor.fetchall()
 for r in results:
     print(r)
--- UpdateDB.py
+++ UpdateDB.py
@@ -26,7 +26,7 @@ class UpdateDB:
         cursor.execute("""SELECT ROUTE_ID, DIRECTION, STOP_ID
         FROM STOPS JOIN STOPS_ON_ROUTES USING(STOP_ID)
                     JOIN ROUTES USING(ROUTE_ID)
-        WHERE ROUTE_ID = '71A' OR ROUTE_ID = '71C' OR ROUTE_ID = '82' OR ROUTE_ID = '75'
+        WHERE ROUTE_ID = '71A' OR ROUTE_ID = '71C' OR ROUTE_ID = '82' OR ROUTE_ID = '75' OR ROUTE_ID = '28X' OR ROUTE_ID = '65'
         """)
         results = cursor.fetchall()
         connection.commit()
