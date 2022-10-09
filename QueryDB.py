5a4cee747c74f53aed208ab2fe138d7c57bc7396
jackvandeleuv <106289788+jackvandeleuv@users.noreply.github.com>
2022-10-08 20:31:33 -0400

    Expanded test db

--- QueryDB.py
+++ QueryDB.py
@@ -9,7 +9,7 @@ connection = sqlite3.Connection('transit_data.db')
 cursor = connection.cursor()
 
 # cursor.execute("""SELECT AVG(ETA) FROM ESTIMATES WHERE ETA != 'DUE' AND STOP_ID = 8192 AND ROUTE_ID = '71A'""")
-cursor.execute('SELECT * FROM ESTIMATES')
+cursor.execute('SELECT ROUTE_ID, AVG(ETA), COUNT(ID) FROM ESTIMATES GROUP BY ROUTE_ID ORDER BY AVG(ETA)')
 results = cursor.fetchall()
 for r in results:
     print(r)
