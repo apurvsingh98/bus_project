66dcae75a8bda2ccc3c406addf12684aeff1077c
jackvandeleuv <106289788+jackvandeleuv@users.noreply.github.com>
2022-10-09 11:46:26 -0400

    Expanded test db

--- QueryDB.py
+++ QueryDB.py
@@ -4,14 +4,31 @@ import sqlite3
 # Knights of Ni Project
 
 
-# Here's the boilerplate code needed to query the database.
-connection = sqlite3.Connection('transit_data.db')
-cursor = connection.cursor()
+class QueryDB:
+    def __init__(self):
+        pass
 
-# cursor.execute("""SELECT AVG(ETA) FROM ESTIMATES WHERE ETA != 'DUE' AND STOP_ID = 8192 AND ROUTE_ID = '71A'""")
-cursor.execute('SELECT ROUTE_ID, AVG(ETA), COUNT(ID) FROM ESTIMATES GROUP BY ROUTE_ID ORDER BY AVG(ETA)')
-results = cursor.fetchall()
-for r in results:
-    print(r)
 
-connection.commit()
+    @staticmethod
+    def count_data():
+        # Here's the boilerplate code needed to query the database.
+        connection = sqlite3.Connection('transit_data.db')
+        cursor = connection.cursor()
+
+        # cursor.execute("""SELECT AVG(ETA) FROM ESTIMATES WHERE ETA != 'DUE' AND STOP_ID = 8192 AND ROUTE_ID = '71A'""")
+        # cursor.execute("""SELECT ROUTE_ID, STOP_ID, STOP_NAME, DIRECTION, AVG(ETA), COUNT(ID)
+        # FROM ESTIMATES JOIN STOPS USING(STOP_ID)
+        # WHERE ROUTE_ID = "71A"
+        # GROUP BY stop_id
+        # ORDER BY STOP_NAME DESC""")
+
+        cursor.execute("""SELECT * FROM ESTIMATES WHERE ROUTE_ID = "71C" """)
+
+        results = cursor.fetchall()
+        for r in results:
+            print(r)
+
+        connection.commit()
+
+
+QueryDB.count_data()
