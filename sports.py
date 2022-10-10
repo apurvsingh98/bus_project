2962527e22f474fef53698d7e7d826f2b7713a82
gmoellering <genna.moellering@gmail.com>
2022-10-09 23:20:54 -0400

    Finished the code that now returns a dictionary of the date and stadium location for all Steeler, Penguins, and University of Pittsburgh Football home games.

--- sports.py
+++ sports.py
@@ -3,11 +3,13 @@ import os
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
 
-# This code returns a dictionary of the date and stadium location for all Steeler's home games.
-# It also returns a list of just the dates of Steeler's home games.
+
+date_conversion = dict(zip(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']))
 
 #Steeler's Schedule
+'Pittsburgh Steeler\'s Home Schedule:'
 httpSteelers = 'https://www.steelers.com/schedule/'
 print(httpSteelers)
 Steelpage = requests.get(httpSteelers)
@@ -24,11 +26,12 @@ location_list = list(game.getText().strip() for game in game_location)
 all_games = dict(zip(game_dates, location_list))
 
 Steelers_home_games = {date:stadium for date,stadium in all_games.items() if stadium == 'Acrisure Stadium'}
-Steelers_home_dates = list(date for date in Steelers_home_games.keys() if date != '')
-print(Steelers_home_games)
-print(Steelers_home_dates, '\n')
+Steelers_home_dates = list('2022/' + date for date in Steelers_home_games.keys() if date != '')
+print(Steelers_home_games, '\n')
+#print(Steelers_home_dates, '\n')
 #
 # #Penguin's Schedule
+print('Pittsburgh Penguins\' Home Schedule:')
 httpPens = 'https://www.ppgpaintsarena.com/events'
 print(httpPens)
 Penspage = requests.get(httpPens)
@@ -42,18 +45,70 @@ events = dict(zip(all_events, all_event_dates))
 Pens_events = {name:date for name,date in events.items() if name[0:8] == 'Penguins'}
 Pens_home_dates = list(Pens_events.values())
 game_location = list(['PPG Paints Arena']*len(Pens_home_dates))
-Pens_home_games = dict(zip(Pens_home_dates, game_location))
+
+formatted_dates = []
+formatted_months = []
+formatted_days = []
+for date in Pens_home_dates:
+    sub_list = date[:3], date[-2:]
+    month = sub_list[0]
+    if month in date_conversion.keys():
+        month = date_conversion[month]
+        formatted_months.append(month)
+    else: pass
+    day = sub_list[1]
+    if day[0] == ' ':
+        new_day = '0' + day[1]
+        formatted_days.append(new_day)
+    else:
+        formatted_days.append(day)
+formatted_dates = list('2022/' + formatted_months[i] + '/' + formatted_days[i] for i in range(len(formatted_months)))
+Pens_home_games = dict(zip(formatted_dates, game_location))
 print(Pens_home_games, '\n')
-# print(Pens_home_dates)
 
 #University of Pittsburgh Panther's Schedule
+print('University of Pittsburgh Panthers\' Home Schedule:')
+column_list = ['Date', 'Time', 'At', 'Opponent', 'Location']
 Panthers_schedule = pd.read_csv(r'C:\Users\genna\OneDrive\Documents\Carnegie Mellon University\CMU - Fall 2022\90819 - Intermediate Programming with Python\Homeworks\bus_project\pitt_football_homegames.txt')
-Panthers_schedule.to_csv(r'C:\Users\genna\OneDrive\Documents\Carnegie Mellon University\CMU - Fall 2022\90819 - Intermediate Programming with Python\Homeworks\bus_project\pitt_football_homegames.csv', header=None)
-print(Panthers_schedule)
+Panthers_schedule.to_csv(r'C:\Users\genna\OneDrive\Documents\Carnegie Mellon University\CMU - Fall 2022\90819 - Intermediate Programming with Python\Homeworks\bus_project\pitt_football_homegames.csv')
+new_schedule = Panthers_schedule.squeeze()
+game_list = list(new_schedule.loc[i] for i in range(7))
 
-Panther_game_dates = Panthers_schedule['At'].to_list()
-print(Panther_game_dates)
+new_game_list = []
+dates = []
+location = []
 
-#def sports_schedule():
-#    date = input('Please enter a date in YYYY-MM-DD format: ')
-#    pass
+for item in game_list:
+    item.split(' ')
+    if item != '':
+        new_game_list.append(item)
+    else:
+        pass
+    item.strip()
+
+for item in new_game_list:
+    dates.append(item[:6])
+for item in new_game_list:
+    location.append(item[-16:])
+
+formatted_dates = []
+formatted_months = []
+formatted_days = []
+for date in dates:
+    sub_list = date[:3], date[-2:]
+    month = sub_list[0]
+    if month in date_conversion.keys():
+        month = date_conversion[month]
+        formatted_months.append(month)
+    else: pass
+    day = sub_list[1]
+    day.strip()
+    if day[1] == ' ':
+        new_day = '0' + day[0]
+        formatted_days.append(new_day)
+    else:
+        formatted_days.append(day)
+formatted_dates = list('2022/' + formatted_months[i] + '/' + formatted_days[i] for i in range(len(formatted_months)))
+
+Panthers_home_games = dict(zip(formatted_dates, location))
+print(Panthers_home_games)
